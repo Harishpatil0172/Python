a177b8b02aa57c2b938e89379aa1f8571f07887e
Harish Patil <77626222+Harishpatil0172@users.noreply.github.com>
2021-09-07 21:54:19 +0530

    If-else condition

--- If-else condition.py
+++ If-else condition.py
@@ -1,4 +1,6 @@
-var=0
-while(var<10):
-    print(var)
-    var+=1
+# If-else Condition
+num=int(input("Enter the number:"))
+if num>10:
+    print("number greater than 10")
+else:
+    print("number less than 10")
